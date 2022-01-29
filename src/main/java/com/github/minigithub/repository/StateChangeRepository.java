7541cc8307187da265eed3c9a5f329091481278b
aca24 <acasavic2409@gmail.com>
2022-01-29 19:28:13 +0100

    added repository annotation at repository classes

--- src/main/java/com/github/minigithub/repository/StateChangeRepository.java
+++ src/main/java/com/github/minigithub/repository/StateChangeRepository.java
@@ -4,7 +4,9 @@ import com.github.minigithub.model.State;
 import com.github.minigithub.model.StateChange;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StateChangeRepository extends JpaRepository<StateChange, Long> {
 
     StateChange findOneById(Long id);

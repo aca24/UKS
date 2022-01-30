f16b7ebf059841c7a579b90bbd925a318c401de6
tamaraglisic <39412716+tamaraglisic@users.noreply.github.com>
2022-01-30 17:28:32 +0100

    project and milestone crud operations

--- src/main/java/com/github/minigithub/mapper/MilestoneMapper.java
+++ src/main/java/com/github/minigithub/mapper/MilestoneMapper.java
@@ -1,19 +1,47 @@
 package com.github.minigithub.mapper;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import com.github.minigithub.dto.MilestoneDTO;
+import com.github.minigithub.dto.TaskDTO;
 import com.github.minigithub.model.Milestone;
+import com.github.minigithub.model.Task;
 
 public class MilestoneMapper {
 	
 	public static MilestoneDTO toDto (Milestone entity) {
-		return new MilestoneDTO();
-		// TO DO 
-		// setuj DTO objekat
+		MilestoneDTO dto = new MilestoneDTO();
+		dto.setId(entity.getId());
+		dto.setDescription(entity.getDescription());
+		dto.setDueDate(entity.getDueDate());
+		dto.setTitle(entity.getTitle());
+		dto.setState(entity.getState());
+		
+		Collection<TaskDTO> tasks = new ArrayList<TaskDTO>();
+		for(Task tentity: entity.getTasks()) {
+			//tasks.add(TaskMapper.toDto(tentity));
+			tasks.add(new TaskDTO());
+		}
+		dto.setTasks(tasks);
+		return dto;
 	}
 	
 	public static Milestone toEntity (MilestoneDTO dto) {
-		return new Milestone();
-		// TO DO
+		Milestone entity = new Milestone();
+		entity.setId(dto.getId());
+		entity.setDescription(dto.getDescription());
+		entity.setDueDate(dto.getDueDate());
+		entity.setTitle(dto.getTitle());
+		entity.setState(dto.getState());
+		
+		Collection<Task> tasks = new ArrayList<Task>();
+		for(TaskDTO tdto: dto.getTasks()) {
+			//tasks.add(TaskMapper.toEntity(tdto));
+			tasks.add(new Task());
+		}
+		entity.setTasks(tasks);
+		return entity;
 	}
 
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
 INSERT INTO ROLES (name) VALUES ('ROLE_USER');
 
 INSERT INTO USERS (username, password, first_name, last_name, role_id) VALUES ('maca', 'maca', 'maca', 'maca', 1);
+

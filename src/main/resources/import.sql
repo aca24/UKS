e0978ba43f3e9a952d0f014dfe0a8b0579db1102
aca24 <acasavic2409@gmail.com>
2022-01-31 23:48:01 +0100

    modified data in script

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,33 +6,33 @@ INSERT INTO ROLES (name) VALUES ('ROLE_ADMIN');
 INSERT INTO USERS (username, password, first_name, last_name, role_id) VALUES ('nestodrugo', 'nestodrugo', 'nestodrugo', 'nestodrugo', 1);
 
 INSERT INTO MILESTONES (id, description, due_date, state, title) VALUES (1, 'nesto', '2021-07-03 00:00:00', 'MERGED','nesto');
-INSERT INTO MILESTONES (id, description, due_date, state, title) VALUES (2, 'nestodrugo', '2021-11-03 00:00:00', 'CLOSE','nestodrugo');
+INSERT INTO MILESTONES (id, description, due_date, state, title) VALUES (2, 'nestodrugo', '2021-11-03 00:00:00', 'CLOSED','nestodrugo');
 
-INSERT INTO TASKS (id, user_id, milestone_id) VALUES (21, 1, 1);
-INSERT INTO TASKS (id, user_id, milestone_id) VALUES (22, 1, 1);
-INSERT INTO TASKS (id, user_id, milestone_id) VALUES (23, 1, 1);
-INSERT INTO TASKS (id, user_id, milestone_id) VALUES (24, 1, 1);
+INSERT INTO TASKS (id, user_id, milestone_id) VALUES (121, 1, 1);
+INSERT INTO TASKS (id, user_id, milestone_id) VALUES (122, 1, 1);
+INSERT INTO TASKS (id, user_id, milestone_id) VALUES (123, 1, 1);
+INSERT INTO TASKS (id, user_id, milestone_id) VALUES (124, 1, 1);
 
-INSERT INTO EVENTS (id, creation_time, task_id) VALUES (101, '2021-07-03 00:00:00', 21);
-INSERT INTO EVENTS (id, creation_time, task_id) VALUES (102, '2021-11-03 11:11:11', 22);
+INSERT INTO EVENTS (id, creation_time, task_id) VALUES (101, '2021-07-03 00:00:00', 121);
+INSERT INTO EVENTS (id, creation_time, task_id) VALUES (102, '2021-11-03 11:11:11', 122);
 
-INSERT INTO COMMENTS (id, creation_time, task_id, content) VALUES (1, '2021-07-03 00:00:00', 21, 'nesto');
-INSERT INTO COMMENTS (id, creation_time, task_id, content) VALUES (2, '2021-11-03 11:11:11', 22, 'nestodrugo');
+INSERT INTO COMMENTS (id, creation_time, task_id, content) VALUES (1, '2021-07-03 00:00:00', 121, 'nesto');
+INSERT INTO COMMENTS (id, creation_time, task_id, content) VALUES (2, '2021-11-03 11:11:11', 122, 'nestodrugo');
 
-INSERT INTO LABEL_APPLICATIONS (id, creation_time, task_id) VALUES (71, '2021-07-03 00:00:00', 21);
-INSERT INTO LABEL_APPLICATIONS (id, creation_time, task_id) VALUES (72, '2021-11-03 11:11:11', 21);
+INSERT INTO LABEL_APPLICATIONS (id, creation_time, task_id) VALUES (71, '2021-07-03 00:00:00', 121);
+INSERT INTO LABEL_APPLICATIONS (id, creation_time, task_id) VALUES (72, '2021-11-03 11:11:11', 121);
 
 INSERT INTO LABELS (id, name, label_application_id) VALUES (140, 'nesto', 71);
 INSERT INTO LABELS (id, name, label_application_id) VALUES (141, 'nestodrugo', 71);
 INSERT INTO LABELS (id, name, label_application_id) VALUES (142, 'nestotrece', 72);
 
-INSERT INTO STATE_CHANGES (id, creation_time, task_id, new_state) VALUES (11, '2021-05-18 01:12:09', 21, 'OPEN');
-INSERT INTO STATE_CHANGES (id, creation_time, task_id, new_state) VALUES (12, '2021-11-11 15:10:19', 22, 'CLOSE');
-INSERT INTO STATE_CHANGES (id, creation_time, task_id, new_state) VALUES (13, '2021-10-24 10:19:15', 23, 'MERGED');
+INSERT INTO STATE_CHANGES (id, creation_time, task_id, new_state) VALUES (111, '2021-05-18 01:12:09', 121, 'OPEN');
+INSERT INTO STATE_CHANGES (id, creation_time, task_id, new_state) VALUES (112, '2021-11-11 15:10:19', 122, 'CLOSED');
+INSERT INTO STATE_CHANGES (id, creation_time, task_id, new_state) VALUES (113, '2021-10-24 10:19:15', 123, 'MERGED');
 
 
-INSERT INTO TASKS (id,user_id) VALUES (158,1);
-INSERT INTO TASKS (id,user_id) VALUES (159,1);
+INSERT INTO TASKS (id,user_id, milestone_id) VALUES (158,1, 1);
+INSERT INTO TASKS (id,user_id, milestone_id) VALUES (159,1, 1);
 
 INSERT INTO BRANCHES(id,name) VALUES (1,'branch1');
 

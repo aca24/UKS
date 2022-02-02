b719e4ae335def843b10ff00bba2677371deba14
tamaraglisic <39412716+tamaraglisic@users.noreply.github.com>
2022-02-02 14:13:38 +0100

    labelApplication attribute setting

--- src/main/java/com/github/minigithub/mapper/LabelMapper.java
+++ src/main/java/com/github/minigithub/mapper/LabelMapper.java
@@ -1,23 +1,22 @@
 package com.github.minigithub.mapper;
 
-import java.util.ArrayList;
 
 import com.github.minigithub.dto.LabelApplicationDTO;
 import com.github.minigithub.dto.LabelDTO;
 import com.github.minigithub.model.Label;
+import com.github.minigithub.model.LabelApplication;
 
 public class LabelMapper {
 
 	
 	public static LabelDTO toDto (Label entity) {
-		return new LabelDTO(entity.getId(), entity.getName(), new LabelApplicationDTO());
-		// TO DO 
-		// setuj DTO objekat
+		
+		return new LabelDTO(entity.getId(), entity.getName(), new LabelApplicationDTO(entity.getLabelApplication()));
 	}
 	
 	public static Label toEntity (LabelDTO dto) {
-		return new Label();
-		// TO DO
+		
+		return new Label(dto.getId(), dto.getName(), new LabelApplication(dto.getLabelApplication()));
 	}
 	
 }

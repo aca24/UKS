16930bae0b65ae5d5718f8826d84540100d5dede
Aleksandra Jordanović <ana.jordanovic@hotmail.com>
2022-02-04 02:39:19 +0100

    update issue controler

--- src/main/java/com/github/minigithub/service/IssueService.java
+++ src/main/java/com/github/minigithub/service/IssueService.java
@@ -1,5 +1,7 @@
 package com.github.minigithub.service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +10,19 @@ import org.springframework.stereotype.Service;
 import com.github.minigithub.model.Issue;
 import com.github.minigithub.model.PullRequest;
 import com.github.minigithub.repository.IssueRepository;
+import com.github.minigithub.repository.PullRequestRepository;
 
 @Service
 public class IssueService {
 	
 	@Autowired
 	private IssueRepository issueRepo;
+	
+	@Autowired
+	private PullRequestService pullReqService;
+	
+	@Autowired
+	private PullRequestRepository prRepo;
 	
 	public List<Issue> findAll(){
 		return issueRepo.findAll();
@@ -24,9 +33,21 @@ public class IssueService {
 		return issueRepo.findById(id).orElse(null);
 	}
 	
-	public Issue create(Issue entity) {
+	public Issue create(Issue entity, Long id) {
+//		PullRequest pq = pullReqService.findById(158L);
+
+		PullRequest pq = pullReqService.findById(id);
+		ArrayList<PullRequest> prs = new ArrayList<>();
+		prs.add(pq);
+		entity.setPullRequests(prs);
+		
+		Collection<Issue> pullReqIssues = pq.getIssue();
+		pullReqIssues.add(entity);
+		
+		
 	 	try {
-			return issueRepo.save(entity);
+	 		//prRepo.saveAndFlush(pq);
+			return issueRepo.saveAndFlush(entity);
 		} catch (Exception e) {
 			return null;
 		}
